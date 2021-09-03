05216b7aa8f6ba9fef27b3cc51079c6dc08074dd
Devang <devngc@gmail.com>
2021-09-03 04:23:27 -0700

    progress

---
+++ test.py
@@ -0,0 +1,16 @@
+import ifcopenshell
+import ifcopenshell.geom
+import FreeCAD
+
+ifc = ifcopenshell.open("FamilyHouse_AC13.ifc")
+columns = ifc.by_type("IfcColumn")
+item = columns[0]
+
+settings = ifcopenshell.geom.settings()
+settings.set(settings.USE_BREP_DATA, True)
+settings.set(settings.USE_WORLD_COORDS, True)
+ifc_shape = ifcopenshell.geom.create_shape(settings, item)
+ifc_shape = ifc_shape.geometry.brep_data
+print(item.id)
+with open("geometry", "w") as file:
+    file.write(ifc_shape)
