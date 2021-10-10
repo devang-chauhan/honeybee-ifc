1c03df333f556126a87518d07e11c0770450711d
Devang <devngc@gmail.com>
2021-10-10 19:20:42 +0530

    refactor(export): update rooms and gap_closed_rooms generation

--- honeybee_ifc/export.py
+++ honeybee_ifc/export.py
@@ -4,7 +4,7 @@ import pathlib
 
 from typing import List, Tuple
 from ifcopenshell.entity_instance import entity_instance as Element
-from ifcopenshell.file import file as File
+from ladybug_geometry.geometry3d import Polyface3D
 from honeybee.shade import Shade
 from honeybee.room import Room
 from honeybee.model import Model
@@ -24,10 +24,31 @@ def get_ifc_settings() -> ifcopenshell.geom.settings:
     return settings
 
 
-def extract_elements(ifc_file: File,
+def get_rooms(spaces: List[Element], settings: ifcopenshell.geom.settings) -> List[Room]:
+    """Get honeybee rooms from IfcSpace elements."""
+    rooms = []
+    for i, space in enumerate(spaces):
+        polyface = get_polyface3d(space, settings)
+        if not polyface.is_solid:
+            faces = Polyface3D.get_outward_faces(polyface.faces, 0.01)
+            polyface3d = Polyface3D.from_faces(faces, 0.01)
+            room = Room.from_polyface3d('Room_'+str(i), polyface3d)
+            rooms.append(room)
+    return rooms
+
+
+def get_shades(elements: List[Element], settings: ifcopenshell.geom.settings) -> List[Shade]:
+    """Get honeybee shades from Ifc elements."""
+    return [Shade(clean_and_id_string('Shade'), face)
+            for element in elements for face in get_polyface3d(element, settings).faces]
+
+
+def extract_elements(ifc_file_path: pathlib.Path,
                      settings: ifcopenshell.geom.settings) -> Tuple[
                          List[Element], List[Element], List[Element], List[Element]]:
     """Extract elements from an IFC file."""
+
+    ifc_file = ifcopenshell.open(ifc_file_path)
     spaces, windows, doors, slabs = [], [], [], []
 
     elements = ('IfcSpace', 'IfcWindow', 'IfcDoor', 'IfcSlab')
@@ -55,55 +76,42 @@ def extract_elements(ifc_file: File,
     return spaces, windows, doors, slabs
 
 
-def get_rooms(elements: List[Element], settings: ifcopenshell.geom.settings) -> List[Room]:
-    """Get honeybee rooms from IfcSpace elements."""
-    return [Room.from_polyface3d('Room_'+str(count),
-                                 get_polyface3d(element, settings))for count, element in enumerate(elements)]
-
-
-def get_shades(elements: List[Element], settings: ifcopenshell.geom.settings) -> List[Shade]:
-    """Get honeybee shades from Ifc elements."""
-    return [Shade(clean_and_id_string('Shade'), face)
-            for element in elements for face in get_polyface3d(element, settings).faces]
-
-
 @duration
-def export_model(ifc_file_path: pathlib.Path, folder: pathlib.Path = None) -> Model:
+def export_model(
+        ifc_file_path: pathlib.Path, spaces: List[Element], windows: List[Element],
+        doors: List[Element], slabs: List[Element], folder: pathlib.Path = None) -> Model:
     """Export HBJSON from IFC"""
 
     file_name = ifc_file_path.stem
     ifc_file = ifcopenshell.open(ifc_file_path)
 
     hb_rooms, hb_apertures, hb_doors, hb_shades, hb_orphaned_faces = [], [], [], [], []
-
-    # Elements extracted from IFC
-    spaces, windows, doors, slabs = extract_elements(ifc_file, get_ifc_settings())
-
     hb_apertures = get_projected_windows(windows, get_ifc_settings(), ifc_file)
     hb_doors = get_projected_doors(doors, get_ifc_settings(), ifc_file)
     hb_rooms = get_rooms(spaces, get_ifc_settings())
     hb_shades = get_shades(slabs, get_ifc_settings())
 
-    # Export model
     hb_model = Model('House', rooms=hb_rooms, orphaned_faces=hb_orphaned_faces,
                      orphaned_apertures=hb_apertures, orphaned_doors=hb_doors,
                      orphaned_shades=hb_shades)
+
     hb_model.to_hbjson(name=file_name, folder=folder)
+
     return hb_model
 
 
-def export_close_gapped_zones(ifc_file_path: pathlib.Path, folder: pathlib.Path = None) -> Model:
+@duration
+def export_close_gapped_zones(
+        ifc_file_path: pathlib.Path, spaces: List[Element],
+        settings: ifcopenshell.geom.settings, folder: pathlib.Path = None, ) -> Model:
     """Export close gapped zones."""
+
     file_name = ifc_file_path.stem
-    ifc_file = ifcopenshell.open(ifc_file_path)
 
-    hb_rooms = []
-    # TODO: Think about getting this extract_elements out of this function
-    spaces = extract_elements(ifc_file, get_ifc_settings())[0]
-    hb_rooms = get_rooms(spaces, get_ifc_settings())
+    polyfaces = [get_polyface3d(element, settings) for element in spaces]
+    gap_closed_rooms = get_gap_closed_rooms(polyfaces)
 
-    # Export gap closed rooms for zones shell
-    gap_closed_rooms = get_gap_closed_rooms(hb_rooms)
     shell_model = Model('Shell', rooms=gap_closed_rooms)
     shell_model.to_hbjson(name=file_name + '_gap_closed_zones', folder=folder)
+
     return shell_model
