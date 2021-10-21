35d2f056a09c3c1a5e3929c437451cdfd41e9d65
Devang <devngc@gmail.com>
2021-10-21 14:29:36 +0530

    refactor(*): update tests to use pytest.fixture

--- tests/apertures_test.py
+++ tests/apertures_test.py
@@ -1,13 +1,10 @@
 """Testing center point locations and normals for apertures in HBJSONs exported from
 two IFC file."""
-import shutil
-import os
+
+import pytest
 import pathlib
-from honeybee_ifc.to_hbjson import export_model, extract_elements, get_ifc_settings
 from honeybee.model import Model
 
-file_1 = pathlib.Path('tests/assets/ifc/FamilyHouse_AC13.ifc')
-file_2 = pathlib.Path('tests/assets/ifc/SmallOffice_d_IFC2x3.ifc')
 
 verified_house_model = Model.from_hbjson(pathlib.Path(
     'tests/assets/hbjsons/FamilyHouse_AC13.hbjson'))
@@ -15,44 +12,26 @@ verified_office_model = Model.from_hbjson(pathlib.Path(
     'tests/assets/hbjsons/SmallOffice_d_IFC2x3.hbjson'))
 
 
-def test_assets():
-    """Test that the files in assets remain."""
-    assert file_1.stem == 'FamilyHouse_AC13'
-    assert file_2.stem == 'SmallOffice_d_IFC2x3'
-
-
-def test_convert_to_hbjson():
-    """Convert IFC to HBJSONs to a temp folder."""
-    temp_folder = r'tests/assets/temp'
-
-    if os.path.isdir(temp_folder):
-        shutil.rmtree(temp_folder)
-    os.mkdir(temp_folder)
-
-    spaces_1, windows_1, doors_1, slabs_1 = extract_elements(file_1, get_ifc_settings())
-    hbjson1 = export_model(file_1, spaces_1, windows_1, doors_1, slabs_1, temp_folder)
-
-    spaces_2, windows_2, doors_2, slabs_2 = extract_elements(file_2, get_ifc_settings())
-    hbjson2 = export_model(file_2, spaces_2, windows_2, doors_2, slabs_2, temp_folder)
-
-    assert isinstance(hbjson1, Model)
-    assert isinstance(hbjson2, Model)
-
-
-def test_number_of_apertures_house():
-    """Test the number of apertures."""
-
-    house_model = Model.from_hbjson(pathlib.Path(
+@pytest.fixture
+def house_model():
+    return Model.from_hbjson(pathlib.Path(
         'tests/assets/temp/FamilyHouse_AC13.hbjson'))
+
+
+@pytest.fixture
+def office_model():
+    return Model.from_hbjson(pathlib.Path(
+        'tests/assets/temp/SmallOffice_d_IFC2x3.hbjson'))
+
+
+def test_number_of_apertures_house(house_model):
+    """Test the number of apertures."""
 
     assert len(house_model.apertures) == 31
 
 
-def test_center_points_house():
+def test_center_points_house(house_model):
     """Make sure the center point location matches the expected location"""
-
-    house_model = Model.from_hbjson(pathlib.Path(
-        'tests/assets/temp/FamilyHouse_AC13.hbjson'))
 
     verified_center_points = [
         aperture.geometry.center for aperture in verified_house_model.apertures]
@@ -65,11 +44,8 @@ def test_center_points_house():
         assert center.distance_to_point(expected_center) <= 0.01
 
 
-def test_normals_house():
+def test_normals_house(house_model):
     """Meke sure the normal of apertures matches expected normals."""
-
-    house_model = Model.from_hbjson(pathlib.Path(
-        'tests/assets/temp/FamilyHouse_AC13.hbjson'))
 
     verified_normals = [
         aperture.geometry.normal for aperture in verified_house_model.apertures]
@@ -83,27 +59,21 @@ def test_normals_house():
         assert normal.angle(vec) <= 0.01
 
 
-def test_number_of_apertures_office():
+def test_number_of_apertures_office(office_model):
     """Test the number of apertures."""
 
-    offie_model = Model.from_hbjson(pathlib.Path(
-        'tests/assets/temp/SmallOffice_d_IFC2x3.hbjson'))
-
-    assert len(offie_model.apertures) == 80
+    assert len(office_model.apertures) == 80
 
 
-def test_center_points_office():
+def test_center_points_office(office_model):
     """Make sure the center point location matches the expected location"""
-
-    offie_model = Model.from_hbjson(pathlib.Path(
-        'tests/assets/temp/SmallOffice_d_IFC2x3.hbjson'))
 
     verified_center_points = [
         aperture.geometry.center for aperture in verified_office_model.apertures]
 
-    assert len(offie_model.apertures) == len(verified_center_points)
+    assert len(office_model.apertures) == len(verified_center_points)
 
-    for count, aperture in enumerate(offie_model.apertures):
+    for count, aperture in enumerate(office_model.apertures):
         center = aperture.geometry.center
         expected_center = verified_center_points[count]
         # Some of the apertures have more than one near by spaces(zones) and therefore,
@@ -113,28 +83,15 @@ def test_center_points_office():
         assert center.distance_to_point(expected_center) < 0.2
 
 
-def center_normals_office():
+def center_normals_office(office_model):
     """Make sure the normal of apertures matches expected normals."""
-
-    offie_model = Model.from_hbjson(pathlib.Path(
-        'tests/assets/temp/SmallOffice_d_IFC2x3.hbjson'))
 
     verified_normals = [
         aperture.geometry.normal for aperture in verified_office_model.apertures]
 
-    assert len(offie_model.apertures) == len(verified_normals)
+    assert len(office_model.apertures) == len(verified_normals)
 
-    for count, aperture in enumerate(offie_model.apertures):
+    for count, aperture in enumerate(office_model.apertures):
         normal = aperture.geometry.normal.normalize()
         vec = verified_normals[count]
         assert normal.angle(vec) <= 0.01
-
-
-def test_remove_temp():
-    """Remove the temp folder."""
-    temp_folder = r'tests/assets/temp'
-
-    if os.path.isdir(temp_folder):
-        shutil.rmtree(temp_folder)
-
-    assert not os.path.isdir(temp_folder)
--- tests/doors_test.py
+++ tests/doors_test.py
@@ -1,60 +1,37 @@
 """Testing center point locations and normals for doors in HBJSONs exported from
 two IFC file."""
 
-import shutil
-import os
+import pytest
 import pathlib
-from honeybee_ifc.to_hbjson import export_model, extract_elements, get_ifc_settings
 from honeybee.model import Model
 
-file_1 = pathlib.Path('tests/assets/ifc/FamilyHouse_AC13.ifc')
-file_2 = pathlib.Path('tests/assets/ifc/SmallOffice_d_IFC2x3.ifc')
 
 house_model_verified = Model.from_hbjson(pathlib.Path(
     'tests/assets/hbjsons/FamilyHouse_AC13.hbjson'))
-
 office_model_verified = Model.from_hbjson(pathlib.Path(
     'tests/assets/hbjsons/SmallOffice_d_IFC2x3.hbjson'))
 
 
-def test_assets():
-    """Test that the files in assets remain."""
-    assert file_1.stem == 'FamilyHouse_AC13'
-    assert file_2.stem == 'SmallOffice_d_IFC2x3'
-
-
-def test_convert_to_hbjson():
-    """Convert IFC to HBJSONs to a temp folder."""
-    temp_folder = r'tests/assets/temp'
-
-    if os.path.isdir(temp_folder):
-        shutil.rmtree(temp_folder)
-    os.mkdir(temp_folder)
-
-    spaces_1, windows_1, doors_1, slabs_1 = extract_elements(file_1, get_ifc_settings())
-    hbjson1 = export_model(file_1, spaces_1, windows_1, doors_1, slabs_1, temp_folder)
-
-    spaces_2, windows_2, doors_2, slabs_2 = extract_elements(file_2, get_ifc_settings())
-    hbjson2 = export_model(file_2, spaces_2, windows_2, doors_2, slabs_2, temp_folder)
-
-    assert isinstance(hbjson1, Model)
-    assert isinstance(hbjson2, Model)
-
-
-def test_number_of_doors_house():
-    """Test the number of doors."""
-
-    house_model = Model.from_hbjson(pathlib.Path(
+@pytest.fixture
+def house_model():
+    return Model.from_hbjson(pathlib.Path(
         'tests/assets/temp/FamilyHouse_AC13.hbjson'))
+
+
+@pytest.fixture
+def office_model():
+    return Model.from_hbjson(pathlib.Path(
+        'tests/assets/temp/SmallOffice_d_IFC2x3.hbjson'))
+
+
+def test_number_of_doors_house(house_model):
+    """Test the number of doors."""
 
     assert len(house_model.doors) == 21
 
 
-def test_center_points_house():
+def test_center_points_house(house_model):
     """Make sure the center point location matches the expected location"""
-
-    house_model = Model.from_hbjson(pathlib.Path(
-        'tests/assets/temp/FamilyHouse_AC13.hbjson'))
 
     verified_center_points = [
         door.geometry.center for door in house_model_verified.doors]
@@ -67,11 +44,8 @@ def test_center_points_house():
         assert center.distance_to_point(expected_center) <= 0.01
 
 
-def test_normals_house():
+def test_normals_house(house_model):
     """Meke sure the normal of doors matches expected normals."""
-
-    house_model = Model.from_hbjson(pathlib.Path(
-        'tests/assets/temp/FamilyHouse_AC13.hbjson'))
 
     verified_normals = [
         door.geometry.normal for door in house_model_verified.doors]
@@ -84,20 +58,14 @@ def test_normals_house():
         assert normal.angle(vec) <= 0.01
 
 
-def test_number_of_doors_office():
+def test_number_of_doors_office(office_model):
     """Test the number of doors."""
-
-    office_model = Model.from_hbjson(pathlib.Path(
-        'tests/assets/temp/SmallOffice_d_IFC2x3.hbjson'))
 
     assert len(office_model.doors) == 33
 
 
-def test_center_points_office():
+def test_center_points_office(office_model):
     """Make sure the center point location matches the expected location"""
-
-    office_model = Model.from_hbjson(pathlib.Path(
-        'tests/assets/temp/SmallOffice_d_IFC2x3.hbjson'))
 
     verified_center_points = [
         door.geometry.center for door in office_model_verified.doors]
@@ -110,11 +78,8 @@ def test_center_points_office():
         assert center.distance_to_point(expected_center) < 0.01
 
 
-def center_normals_office():
+def center_normals_office(office_model):
     """Meke sure the normal of doors matches expected normals."""
-
-    office_model = Model.from_hbjson(pathlib.Path(
-        'tests/assets/temp/SmallOffice_d_IFC2x3.hbjson'))
 
     verified_normals = [
         door.geometry.normal for door in office_model_verified.doors]
@@ -125,13 +90,3 @@ def center_normals_office():
         normal = door.geometry.normal.normalize()
         vec = verified_normals[count]
         assert normal.angle(vec) <= 0.01
-
-
-def test_remove_temp():
-    """Remove the temp folder."""
-    temp_folder = r'tests/assets/temp'
-
-    if os.path.isdir(temp_folder):
-        shutil.rmtree(temp_folder)
-
-    assert not os.path.isdir(temp_folder)
