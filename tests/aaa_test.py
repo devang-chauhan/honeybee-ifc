22b4431aa58e08cff64120ea12223dd6473bb9aa
tymokvo <tymokvo@gmail.com>
2021-11-10 17:53:43 -0800

    fix(tests): Update tests for container

--- tests/aaa_test.py
+++ tests/aaa_test.py
@@ -5,29 +5,27 @@ import os
 import pathlib
 from honeybee_ifc.model import Model
 from honeybee.model import Model as HBModel
+import pytest
 
-file_1 = pathlib.Path('tests/assets/ifc/FamilyHouse_AC13.ifc')
-file_2 = pathlib.Path('tests/assets/ifc/SmallOffice_d_IFC2x3.ifc')
-
-
-def test_assets():
-    """Test that the files in assets remain."""
-    assert file_1.stem == 'FamilyHouse_AC13'
-    assert file_2.stem == 'SmallOffice_d_IFC2x3'
+# file_1 = pathlib.Path('tests/assets/ifc/FamilyHouse_AC13.ifc')
+# file_2 = pathlib.Path('tests/assets/ifc/SmallOffice_d_IFC2x3.ifc')
 
 
-def test_convert_to_hbjson():
+# def test_assets():
+#     """Test that the files in assets remain."""
+#     assert file_1.stem == 'FamilyHouse_AC13'
+#     assert file_2.stem == 'SmallOffice_d_IFC2x3'
+
+def test_convert_to_hbjson(tmp_path: pathlib.Path,
+                           ifc_family_house: pathlib.Path,
+                           ifc_small_office: pathlib.Path):
     """Convert IFC to HBJSONs to a temp folder."""
     # NOTE: This test takes the longest to run.
 
-    temp_folder = r'tests/assets/temp'
+    temp_folder = tmp_path.joinpath('hbjson')
 
-    if os.path.isdir(temp_folder):
-        shutil.rmtree(temp_folder)
-    os.mkdir(temp_folder)
+    hbjson_1 = Model(ifc_family_house).to_hbjson(target_folder=temp_folder)
+    hbjson_2 = Model(ifc_small_office).to_hbjson(target_folder=temp_folder)
 
-    hbjson_1 = Model(file_1).to_hbjson(target_folder=temp_folder)
-    hbjson_2 = Model(file_2).to_hbjson(target_folder=temp_folder)
-
-    assert pathlib.Path(hbjson_1).name == file_1.stem + '.hbjson'
-    assert pathlib.Path(hbjson_2).name == file_2.stem + '.hbjson'
+    assert pathlib.Path(hbjson_1).name == ifc_family_house.stem + '.hbjson'
+    assert pathlib.Path(hbjson_2).name == ifc_small_office.stem + '.hbjson'
