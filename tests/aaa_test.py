e85f2c8ff86e551e4b717295bbcd8e5ef1e31f76
tymokvo <tymokvo@gmail.com>
2021-11-11 09:57:53 -0800

    test: Add tmp dir fixture

--- tests/aaa_test.py
+++ tests/aaa_test.py
@@ -1,31 +1,17 @@
 """Verify assets and write files to be used by unit tests in other modules."""
 
-import shutil
-import os
 import pathlib
 from honeybee_ifc.model import Model
-from honeybee.model import Model as HBModel
-import pytest
-
-# file_1 = pathlib.Path('tests/assets/ifc/FamilyHouse_AC13.ifc')
-# file_2 = pathlib.Path('tests/assets/ifc/SmallOffice_d_IFC2x3.ifc')
 
 
-# def test_assets():
-#     """Test that the files in assets remain."""
-#     assert file_1.stem == 'FamilyHouse_AC13'
-#     assert file_2.stem == 'SmallOffice_d_IFC2x3'
-
-def test_convert_to_hbjson(tmp_path: pathlib.Path,
+def test_convert_to_hbjson(tmp_path_hbjson: pathlib.Path,
                            ifc_family_house: pathlib.Path,
                            ifc_small_office: pathlib.Path):
     """Convert IFC to HBJSONs to a temp folder."""
     # NOTE: This test takes the longest to run.
 
-    temp_folder = tmp_path.joinpath('hbjson')
-
-    hbjson_1 = Model(ifc_family_house).to_hbjson(target_folder=temp_folder)
-    hbjson_2 = Model(ifc_small_office).to_hbjson(target_folder=temp_folder)
+    hbjson_1 = Model(ifc_family_house).to_hbjson(target_folder=tmp_path_hbjson)
+    hbjson_2 = Model(ifc_small_office).to_hbjson(target_folder=tmp_path_hbjson)
 
     assert pathlib.Path(hbjson_1).name == ifc_family_house.stem + '.hbjson'
     assert pathlib.Path(hbjson_2).name == ifc_small_office.stem + '.hbjson'
