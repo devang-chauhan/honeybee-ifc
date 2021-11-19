73b0a79883e398b30d640a74e2fc10d6c18e6a43
Devang <devngc@gmail.com>
2021-11-19 15:13:51 +0530

    test(conftest): add fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,10 @@
-from honeybee_ifc.model import Model
-from honeybee.model import Model as HBModel
 import pytest
 
 from pathlib import Path
 from tempfile import TemporaryDirectory
+
+from honeybee.model import Model as HBModel
+from honeybee_ifc.model import Model
 
 
 @pytest.fixture(scope='session')
@@ -12,18 +13,8 @@ def asset_path():
 
 
 @pytest.fixture(scope='session')
-def family_house_name():
-    return 'FamilyHouse_AC13'
-
-
-@pytest.fixture(scope='session')
 def small_office_name():
     return 'SmallOffice_d_IFC2x3'
-
-
-@pytest.fixture(scope='session')
-def ifc_family_house(asset_path: Path, family_house_name):
-    return asset_path.joinpath(f'ifc/{family_house_name}.ifc')
 
 
 @pytest.fixture(scope='session')
@@ -39,13 +30,6 @@ def tmp_path_hbjson():
 
 
 @pytest.fixture(scope='session')
-def converted_family_house(tmp_path_hbjson: Path, ifc_family_house: Path):
-    m = Model(ifc_family_house).to_hbjson(tmp_path_hbjson)
-    assert Path(m).name == ifc_family_house.stem + '.hbjson'
-    yield m
-
-
-@pytest.fixture(scope='session')
 def converted_small_office(tmp_path_hbjson: Path, ifc_small_office: Path):
     m = Model(ifc_small_office).to_hbjson(tmp_path_hbjson)
     assert Path(m).name == ifc_small_office.stem + '.hbjson'
@@ -53,10 +37,10 @@ def converted_small_office(tmp_path_hbjson: Path, ifc_small_office: Path):
 
 
 @pytest.fixture(scope='session')
-def house_model(converted_family_house: Path):
-    return HBModel.from_hbjson(converted_family_house)
+def office_model(converted_small_office: Path):
+    return HBModel.from_hbjson(converted_small_office)
 
 
 @pytest.fixture(scope='session')
-def office_model(converted_small_office: Path):
-    return HBModel.from_hbjson(converted_small_office)
+def verified_office_model():
+    return HBModel.from_hbjson('tests/assets/hbjsons/SmallOffice_d_IFC2x3.hbjson')
