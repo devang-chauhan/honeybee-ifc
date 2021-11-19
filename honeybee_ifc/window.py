646baf64e40eb36c3e5a6d91c8a3d44deb8b56df
Devang <devngc@gmail.com>
2021-11-19 13:44:09 +0530

    refactor(window): refactor to use walls workflow

--- honeybee_ifc/window.py
+++ honeybee_ifc/window.py
@@ -2,7 +2,9 @@
 
 import ifcopenshell
 from ifcopenshell.entity_instance import entity_instance as IfcElement
-from ladybug_geometry.geometry3d import Face3D
+from ladybug_geometry.geometry3d import Face3D, LineSegment3D
+from honeybee.aperture import Aperture
+from honeybee.typing import clean_and_id_string
 from .element import Element
 from .opening import Opening
 
@@ -23,9 +25,32 @@ class Window(Element):
     @property
     def opening(self) -> Opening:
         """Honeybee-IFC Element for the IfcOpeningElement of an IfcWindow"""
-        return Opening(self.element.FillsVoids[0].RelatingOpeningElement, self.settings)
+        return Opening(self.element.FillsVoids[0].RelatingOpeningElement)
 
     @property
     def face3d(self) -> Face3D:
         """A Face3D representation."""
         return sorted(self.polyface3d.faces, key=lambda x: x.area, reverse=True)[0]
+
+    def moved_opening_face3d(self) -> Face3D:
+        """Get a simplified Face3D representation that is moved to the center of the opening."""
+        # faces parallel to the plane
+        # we need to do this check because people create openings that are way more
+        # extruded than the thickness of the wall. Which means the face with the largest
+        # area may not be the one that we want.
+        parallel_faces = [
+            face for face in self.opening.polyface3d.faces if
+            face.normal.normalize().is_equivalent(self.face3d.plane.n.normalize(), 0.001)]
+
+        area_sorted_faces = sorted(
+            parallel_faces, key=lambda x: x.area, reverse=True)
+
+        face3d = area_sorted_faces[0]
+
+        line = LineSegment3D.from_end_points(face3d.center, self.polyface3d.center)
+        # move the largest face to the center of the window object.
+        return face3d.move(line.v)
+
+    def to_honeybee(self) -> Aperture:
+        """Get a Honeybee Aperture object."""
+        return Aperture(clean_and_id_string('Aperture'), self.moved_opening_face3d())
