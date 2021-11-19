d5a6f1483f5f4f5f881a6e2d839150a5bbdbb008
Devang <devngc@gmail.com>
2021-11-19 13:44:31 +0530

    feat(space): add capability to generate grids

--- honeybee_ifc/space.py
+++ honeybee_ifc/space.py
@@ -1,7 +1,10 @@
 """Honeybee-IFC Space object."""
 
 import ifcopenshell
+from typing import List
 from ifcopenshell.entity_instance import entity_instance as IfcElement
+from honeybee_radiance.sensorgrid import SensorGrid
+from honeybee.typing import clean_and_id_string
 from .element import Element
 
 
@@ -17,3 +20,15 @@ class Space(Element):
         super().__init__(space, settings)
         self.space = space
         self.settings = settings
+
+    def get_grids(self, offset=0.75, size=0.6) -> List[SensorGrid]:
+        """Generate sensor grids from the floor of the space."""
+
+        faces = []
+        for face in self.polyface3d.faces:
+            if face.normal.z == -1:
+                faces.append(face)
+
+        return SensorGrid.from_face3d(clean_and_id_string('Grid'),
+                                      faces, x_dim=size, y_dim=size,
+                                      offset=offset, flip=True)
