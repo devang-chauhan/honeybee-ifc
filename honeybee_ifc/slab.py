19f0eada04344b8ef644198b1beb740a2765b6a6
Devang <devngc@gmail.com>
2021-11-26 12:48:49 +0530

    refactor(slab): remove settings, add property

--- honeybee_ifc/slab.py
+++ honeybee_ifc/slab.py
@@ -1,9 +1,10 @@
 """Honeybee-IFC Slab object."""
 
-import ifcopenshell
+
 from ifcopenshell.entity_instance import entity_instance as IfcElement
 from honeybee.face import Face as Face
 from honeybee.typing import clean_and_id_string
+from ladybug_geometry.geometry3d import Polyface3D
 from .element import Element
 from typing import List
 
@@ -16,12 +17,15 @@ class Slab(Element):
         settings: An IFC settings object.
     """
 
-    def __init__(self, slab: IfcElement, predefined_type: str,
-                 settings: ifcopenshell.geom.settings = None) -> None:
-        super().__init__(slab, settings)
+    def __init__(self, slab: IfcElement, predefined_type: str) -> None:
+        super().__init__(slab)
         self.slab = slab
         self.predefined_type = predefined_type
-        self.settings = settings or self._settings()
+        self._polyface3d = self.to_polyface3d()
+
+    @property
+    def polyface3d(self) -> Polyface3D:
+        return self._polyface3d
 
     def to_honeybee(self) -> List[Face]:
         """Get a list of Honeybee Face objects for the wall."""
