dfe19f7362bae62b9ed5acd17d86745e2f4e2650
Devang <devngc@gmail.com>
2021-11-26 12:49:24 +0530

    refactor(shade): remove settings, use meshed representation for curved geometry

--- honeybee_ifc/shade.py
+++ honeybee_ifc/shade.py
@@ -16,12 +16,16 @@ class Shade(Element):
         settings: An ifcopenshell.geom.settings object.
     """
 
-    def __init__(self, shade: IfcElement,  settings: ifcopenshell.geom.settings = None):
-        super().__init__(shade, settings)
+    def __init__(self, shade: IfcElement):
+        super().__init__(shade)
         self.shade = shade
-        self.settings = settings or self._settings()
 
     def to_honeybee(self):
         """Convert IFC object to Honeybee shade."""
+
+        if not self.brep_possible():
+            return [HBShade(clean_and_id_string('Shade'), face)
+                    for face in self.to_face3ds()]
+
         return [HBShade(clean_and_id_string('Shade'), face)
-                for face in self.polyface3d.faces]
+                for face in self.to_polyface3d().faces]
