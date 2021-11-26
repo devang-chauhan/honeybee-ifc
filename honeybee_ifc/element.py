b201cf9957d69eb5fada4453e86931869031540c
Devang <devngc@gmail.com>
2021-11-26 12:50:13 +0530

    refactor(element): add methods and a major refactor

--- honeybee_ifc/element.py
+++ honeybee_ifc/element.py
@@ -1,9 +1,15 @@
 """Honeybee-ifc Element object."""
 
+from typing import List
+from ladybug_geometry.geometry3d import Polyface3D, Face3D, Point3D
+
+from ._helper import element_to_shape, get_face3ds_from_shape, element_to_face3ds
+
 import ifcopenshell
-from ladybug_geometry.geometry3d import Polyface3D
+from ifcopenshell import geom
 from ifcopenshell.entity_instance import entity_instance as IfcElement
-from ._helper import get_shape, get_face3ds_from_shape
+import FreeCAD
+import Part
 
 
 class Element:
@@ -16,16 +22,18 @@ class Element:
         settings: An ifcopenshell.geom.settings object.
     """
 
-    def __init__(self, element: IfcElement, settings: ifcopenshell.geom.settings = None):
+    def __init__(self, element: IfcElement, representation: str = 'brep') -> None:
         self.element = element
-        self.settings = settings or self._settings()
-        self._polyface3d = self._get_polyface3d()
+        self.representation = representation
 
     @staticmethod
-    def _settings() -> ifcopenshell.geom.settings:
+    def settings(representation: str = 'brep') -> ifcopenshell.geom.settings:
         settings = ifcopenshell.geom.settings()
         settings.set(settings.USE_WORLD_COORDS, True)
-        settings.set(settings.USE_BREP_DATA, True)
+        if representation == 'brep':
+            settings.set(settings.USE_BREP_DATA, True)
+        elif representation == 'mesh':
+            settings.set(settings.USE_BREP_DATA, False)
         return settings
 
     @property
@@ -38,22 +46,53 @@ class Element:
         """Global id of the IFC element."""
         return self.element.GlobalId
 
-    @property
-    def polyface3d(self):
-        """Ladybug Polyface3D representation."""
-        return self._polyface3d
+    def brep_possible(self) -> None:
+        """Check if the Brep can be created from a Shape."""
 
-    def _get_polyface3d(self) -> Polyface3D:
+        if self.representation == 'brep':
+            shape = geom.create_shape(self.settings(), self.element)
+            occ_shape = shape.geometry.brep_data
+            fc_shape = Part.Shape()
+            fc_shape.importBrepFromString(occ_shape)
+            if len(fc_shape.Faces) < 4:
+                return False
+            return True
+
+        return False
+
+    def to_polyface3d(self) -> Polyface3D:
         """Polyface3D object from an IFC element."""
 
-        shape = get_shape(self.element, self.settings)
+        shape = geom.create_shape(self.settings(), self.element)
+        occ_shape = shape.geometry.brep_data
+        fc_shape = Part.Shape()
+        fc_shape.importBrepFromString(occ_shape)
+        shape = fc_shape
+
         face3ds = get_face3ds_from_shape(shape)
         polyface3d = Polyface3D.from_faces(face3ds, tolerance=0.01)
         # if the Polyface is solid return it or return a new Polyface with all faces
         # flipped
         if polyface3d.is_solid:
             return polyface3d
-        else:
-            faces = Polyface3D.get_outward_faces(polyface3d.faces, 0.01)
-            polyface3d = Polyface3D.from_faces(faces, tolerance=0.01)
-            return polyface3d
+
+        faces = Polyface3D.get_outward_faces(polyface3d.faces, 0.01)
+        polyface3d = Polyface3D.from_faces(faces, tolerance=0.01)
+        return polyface3d
+
+    def to_face3ds(self) -> List[Face3D]:
+        """Get a list of Ladybug Face3D objects for the element."""
+
+        shape = geom.create_shape(self.settings('mesh'), self.element)
+
+        # Indices of vertices per triangle face e.g. [f1v1, f1v2, f1v3, f2v1, f2v2, f2v3, ...]
+        faces = shape.geometry.faces
+        # X Y Z of vertices in flattened list e.g. [v1x, v1y, v1z, v2x, v2y, v2z, ...]
+        verts = shape.geometry.verts
+
+        point3ds = [Point3D(verts[i], verts[i + 1], verts[i + 2])
+                    for i in range(0, len(verts), 3)]
+        face3ds = [Face3D([point3ds[faces[i]], point3ds[faces[i + 1]], point3ds[faces[i + 2]]])
+                   for i in range(0, len(faces), 3)]
+
+        return face3ds
