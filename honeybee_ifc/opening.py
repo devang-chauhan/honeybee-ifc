9c2fbf8e10933b2e70ffa924365c5efe76c35037
Devang <devngc@gmail.com>
2021-11-26 12:49:37 +0530

    refactor(opening): remove settings, add property

--- honeybee_ifc/opening.py
+++ honeybee_ifc/opening.py
@@ -1,8 +1,8 @@
 """Honeybee-IFC Opening object."""
 
 
-import ifcopenshell
 from ifcopenshell.entity_instance import entity_instance as IfcElement
+from ladybug_geometry.geometry3d import Polyface3D
 from .element import Element
 
 
@@ -14,7 +14,11 @@ class Opening(Element):
         settings: An ifcopenshell.geom.settings object.
     """
 
-    def __init__(self, opening: IfcElement,  settings: ifcopenshell.geom.settings = None):
-        super().__init__(opening, settings)
+    def __init__(self, opening: IfcElement):
+        super().__init__(opening)
         self.opening = opening
-        self.settings = settings or self._settings()
+        self._polyface3d = self.to_polyface3d()
+
+    @property
+    def polyface3d(self) -> Polyface3D:
+        return self._polyface3d
