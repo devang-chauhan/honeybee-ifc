843608d964882132107a02615b4b15b3b482df13
Devang <devngc@gmail.com>
2021-11-26 17:32:47 +0530

    refactor(model): enable selective extraction

--- honeybee_ifc/model.py
+++ honeybee_ifc/model.py
@@ -4,8 +4,11 @@ Currently, only supporting IFC files with IFC2x3 schema.
 """
 import pathlib
 import multiprocessing
+from enum import Enum
+from typing import List
 
 import ifcopenshell
+from ifcopenshell.util import element
 from ifcopenshell.util.unit import calculate_unit_scale
 from ifcopenshell.util.placement import get_local_placement
 from ifcopenshell.util.selector import Selector
@@ -20,6 +23,16 @@ from .shade import Shade
 from .space import Space
 
 
+class IfcEntity(Enum):
+    """An enumeration of IFC entities that can be imported."""
+
+    window = 'IfcWindow'
+    door = 'IfcDoor'
+    slab = 'IfcSlab'
+    shade = 'IfcColumn'
+    space = 'IfcSpace'
+
+
 class Model:
     """Honeybee-IFC model.
 
@@ -27,22 +40,30 @@ class Model:
         ifc_file_path: A string. The path to the IFC file.
     """
 
-    def __init__(self, ifc_file_path: str) -> None:
+    def __init__(self, ifc_file_path: str, elements: List[IfcEntity] = None,
+                 extract_walls: bool = False) -> None:
         self.ifc_file_path = self._validate_path(ifc_file_path)
         self.ifc_file = ifcopenshell.open(self.ifc_file_path)
         self.settings = self._ifc_settings()
         self.unit_factor = calculate_unit_scale(self.ifc_file)
-        # self.elements = ('IfcSlab', 'IfcColumn', 'IfcWindow', 'IfcDoor',
-        #                  'IfcSpace')
-        self.elements = ('IfcDoor',)
+        self.elements = self._elements(elements)
         self.spaces = []
         self.doors = []
         self.windows = []
         self.slabs = []
         self.walls = []
         self.shades = []
-        # self._extract_walls()
-        self._extract_elements()
+        self.extract_walls = extract_walls
+        if self.extract_walls:
+            self._extract_walls()
+        if not self.elements:
+            self._extract_elements()
+
+    @staticmethod
+    def _elements(elements):
+        if elements:
+            return (item.value for item in elements)
+        return ()
 
     @staticmethod
     def _validate_path(path: str) -> pathlib.Path:
@@ -66,16 +87,17 @@ class Model:
             self.settings, self.ifc_file, multiprocessing.cpu_count(),
             include=self.elements)
 
+        # TODO: Make the exceptions specific at some point.
         if iterator.initialize():
             while iterator.next():
                 shape = iterator.get()
                 element = self.ifc_file.by_guid(shape.guid)
 
-                # if element.is_a() == 'IfcWindow':
-                #     try:
-                #         self.windows.append(Window(element))
-                #     except Exception as e:
-                #         print("Window", e, element.GlobalId)
+                if element.is_a() == 'IfcWindow':
+                    try:
+                        self.windows.append(Window(element))
+                    except Exception as e:
+                        print("Window", e, element.GlobalId)
 
                 if element.is_a() == 'IfcDoor':
                     try:
@@ -83,32 +105,31 @@ class Model:
                     except Exception as e:
                         print("Door", e, element.GlobalId)
 
-                # elif element.is_a() == 'IfcSlab':
-                #     try:
-                #         self.slabs.append(
-                #             Slab(element, element.PredefinedType))
-                #     except Exception as e:
-                #         print("Slab", e, element.GlobalId)
+                elif element.is_a() == 'IfcSlab':
+                    try:
+                        self.slabs.append(
+                            Slab(element, element.PredefinedType))
+                    except Exception as e:
+                        print("Slab", e, element.GlobalId)
 
-                # elif element.is_a() == 'IfcColumn':
-                #     try:
-                #         self.shades.append(
-                #             Shade(element))
-                #     except Exception as e:
-                #         print("Shade", e, element.GlobalId)
+                elif element.is_a() == 'IfcColumn':
+                    try:
+                        self.shades.append(
+                            Shade(element))
+                    except Exception as e:
+                        print("Shade", e, element.GlobalId)
 
-                # elif element.is_a() == 'IfcSpace':
-                #     try:
-                #         self.spaces.append(Space(element))
-                #     except Exception as e:
-                #         print("Space", e, element.GlobalId)
-
-                else:
-                    raise ValueError(f'Unsupported element type: {element.is_a()}')
+                elif element.is_a() == 'IfcSpace':
+                    try:
+                        self.spaces.append(Space(element))
+                    except Exception as e:
+                        print("Space", e, element.GlobalId)
 
     def _extract_walls(self) -> None:
         """Extract IfcWall elements from the IFC file."""
         # Don't use BREP data here. Which will give original trinagulated meshes.
+        # Since for walls we're not looking for BREP data, we're extracting them
+        # separately
         selector = Selector()
         for element in selector.parse(self.ifc_file, '.IfcWall | .IfcWallStandardCase'):
             if len(element.IsDecomposedBy) > 0:
@@ -133,24 +154,34 @@ class Model:
         print("Starting Honeybee translation! \n")
         faces, apertures, doors, shades, grids = [], [], [], [], []
 
-        for wall in self.walls:
-            faces.extend(wall.to_honeybee())
-        print("walls done")
-        apertures = [window.to_honeybee() for window in self.windows]
-        print("apertures done")
-        doors = [door.to_honeybee() for door in self.doors]
-        print("doors done")
-        for slab in self.slabs:
-            faces.extend(slab.to_honeybee())
-        print("slabs done")
-        shades = []
-        for shade in self.shades:
-            shades.extend(shade.to_honeybee())
-        print("shade done")
-        grids = []
-        for space in self.spaces:
-            grids.append(space.get_grids(size=0.3))
-        print("grids done")
+        if self.extract_walls:
+            for wall in self.walls:
+                faces.extend(wall.to_honeybee())
+            print("walls done")
+
+        if IfcEntity.window.value in self.elements:
+            apertures = [window.to_honeybee() for window in self.windows]
+            print("apertures done")
+
+        if IfcEntity.door.value in self.elements:
+            doors = [door.to_honeybee() for door in self.doors]
+            print("doors done")
+
+        if IfcEntity.slab.value in self.elements:
+            for slab in self.slabs:
+                faces.extend(slab.to_honeybee())
+            print("slabs done")
+
+        if IfcEntity.shade.value in self.elements:
+            for shade in self.shades:
+                shades.extend(shade.to_honeybee())
+            print("shade done")
+
+        if IfcEntity.space.value in self.elements:
+            for space in self.spaces:
+                grids.append(space.get_grids(size=0.3))
+            print("grids done")
+
         hb_model = HBModel('Model', orphaned_faces=faces,
                            orphaned_apertures=apertures, orphaned_doors=doors,
                            orphaned_shades=shades)
